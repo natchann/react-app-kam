4160b700e237015093f94b95e1b93ff9a2969c4e
Nastya <hohlushka.zag@gmail.com>
2019-08-20 18:09:15 +0300

    Минимальный стиль в нав бар
    Остальное пока не трогала

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar =()=>{
     return(
       <nav className='nav'>
-      <div><a href='#s'>Profile</a></div>
+      <div><a href='#s'><span>Profile</span></a></div>
       <div><a href='#s'>Messages</a></div>
       <div><a href='#s'>News</a></div>
       <div><a href='#s'>Music</a></div>
